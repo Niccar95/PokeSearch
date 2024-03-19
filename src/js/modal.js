3408bdb2b8e5a075772358d02db86185dbb81c64
Niccar95 <niccar.95@outlook.com>
2024-03-19 15:29:27 +0100

    added button to close modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,9 +1,19 @@
 export const gameModal = () => {
   const modal = document.getElementById("modal");
   const showModalButton = document.querySelector(".playButton");
-  const hideModalButton = document.getElementById("hideModalButton");
+  const hideModalButton = document.querySelector(".hideModalButton");
 
   showModalButton.addEventListener("click", () => {
     modal.style.display = "block";
+    setTimeout(function () {
+      modal.style.opacity = "1";
+    }, 10);
+  });
+
+  hideModalButton.addEventListener("click", () => {
+    modal.style.opacity = "0";
+    setTimeout(function () {
+      modal.style.display = "none";
+    }, 300);
   });
 };
